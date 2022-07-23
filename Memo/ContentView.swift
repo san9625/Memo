86573b4acb5cd3da42490b9b8b09eee4491be846
san9625 <san303jiburi@gmail.com>
2022-07-23 13:47:15 +0900

    Initial Commit

---
+++ Memo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memo
+//
+//  Created by 吉川創麻 on 2022/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memo/MemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoApp.swift
+//  Memo
+//
+//  Created by 吉川創麻 on 2022/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
